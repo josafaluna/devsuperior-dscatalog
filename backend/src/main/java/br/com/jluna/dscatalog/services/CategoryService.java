7a1ac08d8f879148615ed1717a36d33fe9eabe64
Josafa Luna <josafaluna@gmail.com>
2021-01-16 19:41:57 -0300

    @Transaction readOnly = true

--- backend/src/main/java/br/com/jluna/dscatalog/services/CategoryService.java
+++ backend/src/main/java/br/com/jluna/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.jluna.dscatalog.entities.Category;
 import br.com.jluna.dscatalog.repositories.CategoryRepository;
@@ -14,6 +15,7 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository respository;
 
+	@Transactional(readOnly = true)
 	public List<Category> findAll() {
 		List<Category> list = respository.findAll();
 		return list;
